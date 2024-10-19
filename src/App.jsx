c842a17631a8da23f3eee218807f4c02ae098085
nikitos123321 <nikitasaronov1907@gmail.com>
2024-10-19 10:37:12 +0300

    Search

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Header } from './components/HeaderComponent/HeaderComponent'
+import { HomePage } from './pages/HomePage/HomePage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CatalogPage } from './pages/CatalogPage/CatalogPage'
+import { ProductPage } from './pages/ProductPage/ProductPage'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/catalog' element={<CatalogPage />} />
+          <Route path='/catalog/:id' element={<ProductPage />} />
+        </Routes>
+    </>
+  )
+}
+
+export default App
